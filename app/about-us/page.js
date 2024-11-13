4196720fe95d43eb1d530e3b35c9dd299cb95571
Mike Clinton <mikeclinton2002@gmail.com>
2024-11-13 11:59:05 +0100

    testing

---
+++ app/about-us/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image"
+
+export default function page() {
+    return (
+        <main className="py-8 md:py-12 lg:py-16 lg:px-16 px-2 md:px-8">
+            <h1> Iseowo. the Freelancer market Place for Artisans</h1>
+            <Image width={720} height={380} src="/fx9.jpg" alt="about us picture"/>
+            
+        </main>
+    )
+}
